b660c0a8553cfcce34a2d0519ab77f7c41714204
olramde <rladhkstn8@gmail.com>
2019-09-20 15:45:46 +0900

    first commit

---
+++ src/5597/5597.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    bool arr[30] = {
+        false,
+    };
+    int num;
+    const int MAX_SUM = 465;
+    int sum = 0;
+
+    for (int i = 0; i < 28; i++)
+    {
+        cin >> num;
+        arr[num] = true;
+        sum += num;
+    }
+    sum = MAX_SUM - sum;
+
+    for (int j = 0; j < 30; j++)
+    {
+        if (arr[j + 1] == false)
+        {
+            cout << j + 1 << endl;
+            cout << sum - (j + 1) << endl;
+            return 0;
+        }
+    }
+}
